99fb4bbeea257abfd6c344fd842337a013e32056
David Zulaica <zulaica@gmail.com>
2018-05-09 17:04:36 -0700

    🆕 📦  Hyper - hypercwd, hyper-pane

--- .hyper.js
+++ .hyper.js
@@ -119,7 +119,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hypercwd", "hyper-pane"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
